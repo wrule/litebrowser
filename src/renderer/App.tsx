affe65b75d61c5fd097060b4d0be8ea33d5a796d
wrule <1982775886@qq.com>
2024-04-03 13:50:31 +0800

    界面优化和依赖

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,11 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import zhCN from 'antd/locale/zh_CN';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, Layout, Menu } from 'antd';
 import './App.scss';
 import Home from './views/Home';
+import { ChromeFilled, CodeSandboxSquareFilled, SettingFilled } from '@ant-design/icons';
+
+const { Sider, Header, Content } = Layout;
 
 export default function App() {
   return (
@@ -12,11 +15,22 @@ export default function App() {
         borderRadius: 0,
       },
     }}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible>
+          <Menu theme="dark" defaultSelectedKeys={['browser']}>
+            <Menu.Item key="browser" icon={<ChromeFilled />}>浏览器</Menu.Item>
+            <Menu.Item key="core" icon={<CodeSandboxSquareFilled />}>内核管理</Menu.Item>
+            <Menu.Item key="config" icon={<SettingFilled />}>系统设置</Menu.Item>
+          </Menu>
+        </Sider>
+        <Content>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
+        </Content>
+      </Layout>
     </ConfigProvider>
   );
 }

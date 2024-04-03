917f5fc017be76662db5b0cceac0087945343890
wrule <1982775886@qq.com>
2024-04-03 17:15:42 +0800

    openBrowser

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,6 +1,7 @@
 /* eslint import/prefer-default-export: off */
 import { URL } from 'url';
 import path from 'path';
+import { exec } from 'child_process';
 
 export function resolveHtmlPath(htmlFileName: string) {
   if (process.env.NODE_ENV === 'development') {
@@ -10,4 +11,16 @@ export function resolveHtmlPath(htmlFileName: string) {
     return url.href;
   }
   return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
+}
+
+const chromePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+
+export function
+openBrowser(userDataDir: string) {
+  const command = `"${chromePath}" ${`
+    --flag-switches-begin --flag-switches-end
+  `.trim()} ${`
+    --user-data-dir="${userDataDir}"
+  `.trim()}`;
+  exec(command);
 }

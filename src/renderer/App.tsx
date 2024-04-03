636225248249f0d49c281a9afeedfc800628d48f
wrule <1982775886@qq.com>
2024-04-03 13:32:19 +0800

    提交

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,21 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { ConfigProvider } from 'antd';
 import Home from './views/Home';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <ConfigProvider theme={{
+      token: {
+        colorPrimary: '#346fff',
+        borderRadius: 0,
+      },
+    }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ConfigProvider>
   );
 }
--- src/renderer/views/Home/index.tsx
+++ src/renderer/views/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import style from './index.module.scss';
+import { Button } from 'antd';
 
 export default function Home() {
-  return <div className={style.view}>你好，世界</div>;
+  return <div className={style.view}>
+    <Button type="primary">你好，世界</Button>
+  </div>;
 }

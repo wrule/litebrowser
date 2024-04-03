66379d1506f352f9c384288957b4c6d7310a7b11
wrule <1982775886@qq.com>
2024-04-03 22:59:00 +0800

    提交

--- src/renderer/views/Browser/index.tsx
+++ src/renderer/views/Browser/index.tsx
@@ -1,10 +1,298 @@
-import { Button } from 'antd';
+import { Button, Col, Form, Input, Layout, Row, Space, Table, Tag } from 'antd';
 import { post } from '../../http';
+
+const { Header, Content } = Layout;
 
 export default function Browser() {
   return <div>
-    <Button type="primary" onClick={async () => {
+    <Row className="controller">
+    <Form
+          layout="inline">
+          <Form.Item
+            label="名称">
+            <Input
+              placeholder="请输入名称"
+            />
+          </Form.Item>
+          <Form.Item
+            label="标签">
+            <Input
+              placeholder="请输入名称"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Space>
+              <Button>重置</Button>
+              <Button type="primary">搜索</Button>
+            </Space>
+          </Form.Item>
+        </Form>
+        <Space>
+          <Button type="primary">新增</Button>
+        </Space>
+    </Row>
+    <Row className="row">
+      <Col span={24}>
+        <Table
+          bordered
+          size="small"
+          pagination={false}
+          scroll={{ x: 1200 }}
+          columns={[
+            {
+              title: '序号',
+              width: 70,
+              fixed: 'left',
+              sorter: true,
+            },
+            {
+              title: '名称',
+              fixed: 'left',
+              width: 300,
+            },
+            {
+              title: '标签',
+              render: (row) => {
+                return <Space>
+                  <Tag>1234</Tag>
+                  <Tag>1234</Tag>
+                </Space>;
+              },
+            },
+            {
+              title: '最近打开',
+              width: 170,
+              fixed: 'right',
+              sorter: true,
+              render: (row) => {
+                return <span>2024-05-31 18:24:56</span>;
+              },
+            },
+            {
+              title: '操作',
+              width: 120,
+              fixed: 'right',
+              render: (row) => {
+                return <Space>
+                  <Button size="small" type="primary">打开</Button>
+                </Space>;
+              },
+            },
+          ]}
+          dataSource={[{ }, { }, { }]}
+        />
+      </Col>
+    </Row>
+    <Row className="row">
+      <Col span={24}>
+        <Table
+          bordered
+          size="small"
+          pagination={false}
+          scroll={{ x: 1200 }}
+          columns={[
+            {
+              title: '序号',
+              width: 70,
+              fixed: 'left',
+              sorter: true,
+            },
+            {
+              title: '名称',
+              fixed: 'left',
+              width: 300,
+            },
+            {
+              title: '标签',
+              render: (row) => {
+                return <Space>
+                  <Tag>1234</Tag>
+                  <Tag>1234</Tag>
+                </Space>;
+              },
+            },
+            {
+              title: '最近打开',
+              width: 170,
+              fixed: 'right',
+              sorter: true,
+              render: (row) => {
+                return <span>2024-05-31 18:24:56</span>;
+              },
+            },
+            {
+              title: '操作',
+              width: 120,
+              fixed: 'right',
+              render: (row) => {
+                return <Space>
+                  <Button size="small" type="primary">打开</Button>
+                </Space>;
+              },
+            },
+          ]}
+          dataSource={[{ }, { }, { }]}
+        />
+      </Col>
+    </Row>
+    <Row className="row">
+      <Col span={24}>
+        <Table
+          bordered
+          size="small"
+          pagination={false}
+          scroll={{ x: 1200 }}
+          columns={[
+            {
+              title: '序号',
+              width: 70,
+              fixed: 'left',
+              sorter: true,
+            },
+            {
+              title: '名称',
+              fixed: 'left',
+              width: 300,
+            },
+            {
+              title: '标签',
+              render: (row) => {
+                return <Space>
+                  <Tag>1234</Tag>
+                  <Tag>1234</Tag>
+                </Space>;
+              },
+            },
+            {
+              title: '最近打开',
+              width: 170,
+              fixed: 'right',
+              sorter: true,
+              render: (row) => {
+                return <span>2024-05-31 18:24:56</span>;
+              },
+            },
+            {
+              title: '操作',
+              width: 120,
+              fixed: 'right',
+              render: (row) => {
+                return <Space>
+                  <Button size="small" type="primary">打开</Button>
+                </Space>;
+              },
+            },
+          ]}
+          dataSource={[{ }, { }, { }]}
+        />
+      </Col>
+    </Row>
+    <Row className="row">
+      <Col span={24}>
+        <Table
+          bordered
+          size="small"
+          pagination={false}
+          scroll={{ x: 1200 }}
+          columns={[
+            {
+              title: '序号',
+              width: 70,
+              fixed: 'left',
+              sorter: true,
+            },
+            {
+              title: '名称',
+              fixed: 'left',
+              width: 300,
+            },
+            {
+              title: '标签',
+              render: (row) => {
+                return <Space>
+                  <Tag>1234</Tag>
+                  <Tag>1234</Tag>
+                </Space>;
+              },
+            },
+            {
+              title: '最近打开',
+              width: 170,
+              fixed: 'right',
+              sorter: true,
+              render: (row) => {
+                return <span>2024-05-31 18:24:56</span>;
+              },
+            },
+            {
+              title: '操作',
+              width: 120,
+              fixed: 'right',
+              render: (row) => {
+                return <Space>
+                  <Button size="small" type="primary">打开</Button>
+                </Space>;
+              },
+            },
+          ]}
+          dataSource={[{ }, { }, { }]}
+        />
+      </Col>
+    </Row>
+    <Row className="row">
+      <Col span={24}>
+        <Table
+          bordered
+          size="small"
+          pagination={false}
+          scroll={{ x: 1200 }}
+          columns={[
+            {
+              title: '序号',
+              width: 70,
+              fixed: 'left',
+              sorter: true,
+            },
+            {
+              title: '名称',
+              fixed: 'left',
+              width: 300,
+            },
+            {
+              title: '标签',
+              render: (row) => {
+                return <Space>
+                  <Tag>1234</Tag>
+                  <Tag>1234</Tag>
+                </Space>;
+              },
+            },
+            {
+              title: '最近打开',
+              width: 170,
+              fixed: 'right',
+              sorter: true,
+              render: (row) => {
+                return <span>2024-05-31 18:24:56</span>;
+              },
+            },
+            {
+              title: '操作',
+              width: 120,
+              fixed: 'right',
+              render: (row) => {
+                return <Space>
+                  <Button size="small" type="primary">打开</Button>
+                </Space>;
+              },
+            },
+          ]}
+          dataSource={[{ }, { }, { }]}
+        />
+      </Col>
+    </Row>
+    {/* <Button type="primary" onClick={async () => {
       const res: any = await post('/openBrowser', { userDataDir: '/Users/jimao/github/cbmgr/0' });
-    }}>点我</Button>
+    }}>点我</Button> */}
   </div>;
 }

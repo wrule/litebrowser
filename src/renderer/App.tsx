f2f6d32d44721020da692e2fe834ef6104d2091b
wrule <1982775886@qq.com>
2024-04-03 13:14:12 +0800

    title和依赖

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
-import './App.css';
+import './App.scss';
 
 function Hello() {
   return (
